adf8441fc7864193170c2847d950016dfa19ce60
David Sulpy <david@sulpy.com>
2015-12-02 17:39:03 -0600

    Added satellite count and only streaming location and altitude when sat count is greater than or equal to 3 and 4 respectively

--- app.py
+++ app.py
@@ -15,7 +15,10 @@ try:
 		sentence = serialStream.readline()
 		if sentence.find('GGA') > 0:
 			data = pynmea2.parse(sentence)
-			streamer.log("Location", "{lat},{lon}".format(lat=data.latitude,lon=data.longitude))
-			streamer.log("Altitude ({unit})".format(unit=data.altitude_units), data.altitude)
+			streamer.log("Satellite Count", data.num_sats)
+			if (data.num_sats >= 3):
+				streamer.log("Location", "{lat},{lon}".format(lat=data.latitude,lon=data.longitude))
+			if (data.num_sats >= 4):
+				streamer.log("Altitude ({unit})".format(unit=data.altitude_units), data.altitude)
 except KeyboardInterrupt:
 	streamer.close()
